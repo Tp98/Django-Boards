300e28a6a19a2901bcf04442973dc3179828096e
Alexandre BOTTE <C00243743@itcarlow.ie>
2019-01-22 15:46:58 +0000

    Modified the views.py to print the two names of boards (Django & Python)

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Hello, World!')
+from .models import Board
 
+def home(request):
+    boards = Board.objects.all()
+    boards_name = list()
+
+    for board in boards:
+        boards_name.append(board.name)
+
+    response_html = '<br>'.join(boards_name)
+
+    return HttpResponse(response_html)
 # Create your views here.
