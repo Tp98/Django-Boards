6646d1b7f990b91adb678f32457a178f8674af64
Alexandre BOTTE <C00243743@itcarlow.ie>
2019-01-24 14:33:54 +0000

    Put links to topics on board name

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -5,6 +5,11 @@ from .models import Board
 # Create your tests here.
 
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -12,7 +17,12 @@ class HomeTests(TestCase):
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)    
+        self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk' : self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+   
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
