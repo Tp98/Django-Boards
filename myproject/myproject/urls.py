2eb5610d624ed52166a0baeb25e19f7b76fae30d
Alexandre BOTTE <C00243743@itcarlow.ie>
2019-02-01 17:34:00 +0000

    Implements the log out and a dropdown if we are login or two buttons to sign up or login if we are not

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('boards/<int:pk>/', views.board_topics, name="board_topics"),
     path('boards/<int:pk>/new/', views.new_topic,name='new_topic'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
