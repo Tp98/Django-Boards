41dd5195faca36c44716d01ce1ab07f2199789ab
Alexandre BOTTE <C00243743@itcarlow.ie>
2019-02-12 14:52:54 +0000

    black formated code

--- myproject/accounts/tests/test_form_signup.py
+++ myproject/accounts/tests/test_form_signup.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from ..forms import SignupForm
 
+
 class SignUpFormTest(TestCase):
     def test_form_has_fields(self):
         form = SignupForm()
-        expected = ['username','email','password1','password2',]
+        expected = ["username", "email", "password1", "password2"]
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
-        
--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -9,22 +9,23 @@ from django.views.generic import UpdateView
 
 # Create your views here.
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = SignupForm()
-    return render(request, 'signup.html', {'form':form})
+    return render(request, "signup.html", {"form": form})
 
-@method_decorator(login_required, name='dispatch')
+
+@method_decorator(login_required, name="dispatch")
 class UserUpdateView(UpdateView):
     model = User
-    fields = ('first_name', 'last_name', 'email')
-    template_name = 'my_account.html'
-    success_url = reverse_lazy('my_account')
+    fields = ("first_name", "last_name", "email")
+    template_name = "my_account.html"
+    success_url = reverse_lazy("my_account")
 
     def get_object(self):
         return self.request.user
